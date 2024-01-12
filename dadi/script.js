23c58157cbe8f5b946c3434ac357d03b8a5bc89f
nicola1397 <nicola1397@outlook.it>
2024-01-12 18:38:52 +0100

    refactor: changed from alerts to text

--- dadi/script.js
+++ dadi/script.js
@@ -1,24 +1,33 @@
 const userDice = document.getElementById("playerDice");
 const cpuDice = document.getElementById("cpuDice");
+const roll = document.getElementById("roll");
+const result = document.getElementById("result");
+let toggle = false;
+let temp1;
+let temp2;
 
-userDice.addEventListener("click", function () {
+roll.addEventListener("click", function () {
   gen1 = Math.floor(Math.random() * 6 + 1);
   console.log(gen1);
   gen2 = Math.floor(Math.random() * 6 + 1);
   console.log(gen2);
-
-  userDice.classList.replace("bi-dice-1-fill", `bi-dice-${gen1}-fill`);
-  cpuDice.classList.replace("bi-dice-1", `bi-dice-${gen2}`);
-
-  setTimeout(() => {
-    if (gen1 > gen2) {
-      alert("HAI VINTO!");
-    } else if (gen1 < gen2) {
-      alert("HAI PERSO!");
-    } else {
-      alert("PAREGGIO!");
-    }
-    userDice.classList.replace(`bi-dice-${gen1}-fill`, "bi-dice-1-fill");
-    cpuDice.classList.replace(`bi-dice-${gen2}`, "bi-dice-1");
-  }, 500);
+  if (toggle == false) {
+    userDice.classList.replace("bi-dice-1-fill", `bi-dice-${gen1}-fill`);
+    cpuDice.classList.replace("bi-dice-1", `bi-dice-${gen2}`);
+    temp1 = gen1;
+    temp2 = gen2;
+    toggle = true;
+  } else {
+    userDice.classList.replace(`bi-dice-${temp1}-fill`, `bi-dice-${gen1}-fill`);
+    cpuDice.classList.replace(`bi-dice-${temp2}`, `bi-dice-${gen2}`);
+    temp1 = gen1;
+    temp2 = gen2;
+  }
+  if (gen1 > gen2) {
+    result.innerHTML = `<h2 class="display-3 fw-bold text-center text-success" id="result">HAI VINTO!</h2>`;
+  } else if (gen1 < gen2) {
+    result.innerHTML = `<h2 class="display-3 fw-bold text-center text-danger" id="result">HAI PERSO!</h2>`;
+  } else {
+    result.innerHTML = `<h2 class="display-3 fw-bold text-center text-warning" id="result">PAREGGIO!</h2>`;
+  }
 });
