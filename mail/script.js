0e08a55e71ffd9e7001666c75ab8d0ca58782ced
nicola1397 <nicola1397@outlook.it>
2024-01-13 16:19:39 +0100

    refactor: removed cycle break, added token

--- mail/script.js
+++ mail/script.js
@@ -7,19 +7,21 @@ const allowedUsers = [
 ];
 
 button.addEventListener("click", function () {
+  let accessToken = false;
   const email = document.getElementById("emailAddress").value;
   console.log(email);
 
   for (let i = 0; i < allowedUsers.length; i++) {
     if (email == allowedUsers[i]) {
-      console.log(email, "match non trovato!");
-      access.innerHTML = `
-        <h1 class="text-success fw-bold ms-4">Accesso Consentito</h1>`;
-      break;
-    } else {
-      console.log(email, "match non trovato!");
-      access.innerHTML = `
-        <h1 class="text-danger fw-bold ms-4">Accesso Negato</h1>`;
+      accessToken = true;
     }
+  }
+
+  if (accessToken == true) {
+    access.innerHTML = `
+          <h1 class="text-success fw-bold ms-4">Accesso Consentito</h1>`;
+  } else {
+    access.innerHTML = `
+          <h1 class="text-danger fw-bold ms-4">Accesso Negato</h1>`;
   }
 });
